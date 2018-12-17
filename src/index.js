68ee6b62097638482ca20aee9c737355770a7290
Matthew Wright <matthewbwright@gmail.com>
2018-12-17 12:30:36 -0700

    sets up call to YouTube

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCU8eL1_YNmsC0DZV_d9GHfJAhu-hbFQ2c';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML.
 
