5172eb71724aade7c9176f907e65ad928dca3f38
Matthew Wright <matthewbwright@gmail.com>
2018-12-17 11:16:17 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCU8eL1_YNmsC0DZV_d9GHfJAhu-hbFQ2c';
+
+// Create a new component. This component should produce some HTML.
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+// Take this component's generated HTML and put it on the page (in the DOM).
+
+ReactDOM.render(<App />, document.querySelector('.container'));
