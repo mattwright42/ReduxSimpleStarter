73c24fcd68836995947b1bcf3add98ecb0f41245
Matthew Wright <matthewbwright@gmail.com>
2018-12-18 10:31:45 -0700

    adds VideoDetail

---
+++ src/components/video_detail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" />
+      </div>
+      <div className="deatils">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.decscription}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
