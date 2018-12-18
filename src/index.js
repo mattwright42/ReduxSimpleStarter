3c97510a6371a0ba0e712c5d464c3febe6419b61
Matthew Wright <matthewbwright@gmail.com>
2018-12-18 10:54:28 -0700

    passes props for ability to click videos

--- src/index.js
+++ src/index.js
@@ -20,7 +20,10 @@ class App extends Component {
     };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
       console.log(videos);
     });
   }
@@ -29,7 +32,10 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
